3a4b81c6a08cddc45e408f55ef71c752f1c287c9
Sieunguoimay <vuduydu1997@gmail.com>
2020-08-22 23:14:40 +0700

    h

--- app/Http/Controllers/OnluckController.php
+++ app/Http/Controllers/OnluckController.php
@@ -325,7 +325,7 @@ class OnluckController extends Controller
             $user->current_question_indices = CurrentQuestionIndex::where('playing_data_id',$playingData->id)->whereIn('pack_id',$packs)->pluck('index');
             $user->correct_answer_count = count(QuestionPlayingData::select('status')
                     ->where([['playing_data_id','=',$playingData->id],['season_id','=',$season_id],['status','=',99]])->get());
-            unset($user->created_at);        
+            unset($user->updated_at);        
         }
 
         $response["data"] = $users;
